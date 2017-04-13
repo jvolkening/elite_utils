59f4009abc8aeb9ec13d261a31dfb098bf1f6b47
jvolkening <jdv@base2bio.com>
2017-04-12 20:44:36 -0500

    optionally read user, mzml, mgf values from RAW file

--- cp_raw.pl
+++ cp_raw.pl
@@ -6,6 +6,7 @@ use warnings;
 use 5.012;
 
 use Digest::MD5;
+use Encode qw/decode/;
 use File::Basename qw/basename/;
 use File::Path qw/make_path/;
 use File::Copy;
@@ -14,10 +15,12 @@ use Getopt::Long;
 use Time::Piece;
 
 my $DEST_DIR = "T:/incoming";
+my $DEFAULT_USER = 'other';
 
 my $fn_raw;
-my $user = 'other';
-my $mzml = 0;
+my $user;
+my $mzml;
+my $mgf;
 
 GetOptions(
     'raw=s'  => \$fn_raw,
@@ -28,6 +31,21 @@ GetOptions(
 die "ERROR: No such RAW file or file not readable\n"
     if (! -r $fn_raw);
 
+my ($filename, $head_user, $head_mzml, $head_mgf) = parse_raw($fn_raw);
+
+$user = defined $user      ? $user
+      : defined $head_user ? $head_user
+      : $DEFAULT_USER;
+
+$mzml = defined $mzml      ? $mzml
+      : defined $head_mzml ? $head_mzml
+      : 0;
+
+$mgf  = defined $mgf      ? $mgf
+      : defined $head_mgf ? $head_mgf
+      : 0;
+
+$user =~ s/\s/_/g;
 die "Invalid user name (max 16 chars w/ only alphanumerics and underscore)\n"
     if ($user =~ /\W/ || length($user) > 16);
 
@@ -35,8 +53,9 @@ my $base     = basename($fn_raw);
 my $fn_dest  = "$DEST_DIR/$base";
 my $fn_ready = "$DEST_DIR/$base.ready";
 
-# make sure value of $mzml is numeric
+# make sure values of $mzml and $mgf are numeric
 $mzml = $mzml ? 1 : 0;
+$mgf  = $mgf  ? 1 : 0;
 
 die "ERROR: $fn_ready exists and won't overwrite\n"
     if (-e $fn_ready);
@@ -62,6 +81,7 @@ open my $ready, '>', $fn_ready
 say {$ready} "user=", $user;
 say {$ready} "time=", localtime()->datetime;
 say {$ready} "mzml=", $mzml;
+say {$ready} "mgf=",  $mgf;
 say {$ready} "type=", 'raw';
 say {$ready} "md5=",  $dig->hexdigest;
 say {$ready} "size=", $size;
@@ -72,3 +92,60 @@ close $ready;
 
 exit;
 
+
+
+sub parse_raw {
+
+    my ($fn) = @_;
+
+    open my $raw, '<:raw', $fn;
+
+    my $magic = read_bin($raw => 'v');
+    die "Bad magic\n" if ($magic != 0xa101);
+    my $sig = decode('UTF-16LE', read_bin($raw => 'a18') );
+    die "Bad sig\n" if ($sig ne "Finnigan\0");
+
+    # skip rest of header
+    seek STDIN, 1336, 1;
+    # skip injection data
+    seek STDIN, 64, 1;
+
+    my @fields = map {read_pascal($raw)} 1..13;
+
+    my $user_1    = $fields[ 4];
+    my $user_2    = $fields[ 5];
+    my $user_3    = $fields[ 6];
+    my $filename  = $fields[11];
+    my $filepath  = $fields[12];
+
+    $filename = "$filepath/" . basename($filename);
+    die "Parsed filename not found\n"
+        if (! -e $filename);
+
+    return ($filename, $user_1, $user_2, $user_3);
+
+}
+
+sub read_bin {
+
+    my ($fh, $type) = @_;
+    my $len = length(pack($type,()));
+    my $r = read($fh, my $buf, $len);
+    die "Wrong number of bytes read (want $len, got $r)\n"
+        if ($r != $len);
+
+   return unpack "$type*", $buf;
+
+}
+
+sub read_pascal {
+
+    my ($fh) = @_;
+    my $buf;
+    my $r = read $fh, $buf, 4;
+    my $n_char = unpack 'V', $buf;
+
+    $r = read $fh, $buf, $n_char * 2;
+    return decode('UTF-16LE', $buf);
+
+}
