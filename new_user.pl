b6adf60270f0ad2a4ade7ffd72a2160ec02c34f7
jvolkening <jdv@base2bio.com>
2017-04-23 23:40:10 -0500

    updates to new_user.pl

--- new_user.pl
+++ new_user.pl
@@ -8,47 +8,112 @@ use Bio::Galaxy::API;
 use Email::Valid;
 use Net::SMTP::SSL;
 use Getopt::Long;
+use Net::Domain qw/hostfqdn/;
 
 my $name;
+my $user;
 my $email;
-my $server;
+my $url;
 my $org = 'local';
 my $pw_len = 10;
 my @cc;
+my $template;
 
 GetOptions(
-    'name=s'   => \$name,
-    'email=s'  => \$email,
-    'server=s' => \$server,
-    'org=s'    => \$org,
-    'pw_len=i' => \$pw_len,
-    'cc=s'     => \@cc,
+    'name=s'     => \$name,
+    'user=s'     => \$user,
+    'email=s'    => \$email,
+    'url=s'      => \$url,
+    'org=s'      => \$org,
+    'pw_len=i'   => \$pw_len,
+    'cc=s'       => \@cc,
+    'template=s' => \$template,
 );
+
+$url //= 'http://localhost:8080';
+
+if (! defined $user) {
+    $user = lc $name;
+    $user =~ s/\s+/_/g;
+    pos($user) = 0;
+    $user =~ s/[^a-z0-9\_\-]/-/g;
+    say "No username provided, using $user\n";
+}
 
 # validation
 die "missing name or email\n"
     if (! defined $name || ! defined $email);
+
+die "missing or unreadable mail template\n"
+    if (! -r $template);
 
 die "Bad email\n"
     if (! Email::Valid->address($email));
 
 my $pw = random_pw( $pw_len );
 
-create_galaxy_user($name, $email, $pw);
-send_mail($name, $email, $pw, @cc);
+#create_galaxy_user($user, $email, $pw, $url);
+my $msg = generate_email_text(
+    $template, $name, $user, $email, $org, $pw
+);
+send_mail($msg, $name, $email, $pw, @cc);
 
 exit;
 
 sub create_galaxy_user {
 
+    my ($user, $email, $pw, $url) = @_;
+
+    my $check_secure = $url eq 'http://localhost:8080'
+        ? 0
+        : 1;
+
+    my $ua = Bio::Galaxy::API->new(
+        url => $url,
+        check_secure => $check_secure,
+    );
+
+    my $usr = $ua->new_user(
+        user     => $user,
+        email    => $email,
+        password => $pw,
+    );
+
+    if (defined $usr) {
+        say "Successfully created Galaxy user\n";
+    }
+    else {
+        say "Error creating Galaxy user\n";
+        exit;
+    }
+
 }
 
 sub send_mail {
 
-    my ($name, $email, $pw, @cc) = @_;
+    my ($msg, $name, $email, $pw, @cc) = @_;
 
-    my $msg = generate_email_text($name, $email, $pw);
-    say "$msg";
+    my $sender = $ENV{USER} . '@' . hostfqdn();
+
+    my $smtp = Net::SMTP->new('localhost')
+        or die "Error starting SMTP session: $@\n";
+    $smtp->mail($sender);
+    $smtp->to($email);
+    $smtp->cc(@cc);
+
+    $smtp->data();
+    $smtp->datasend("To: $email\n");
+    $smtp->datasend("From: $sender\n");
+    $smtp->datasend("Subject: Galaxy account creation\n");
+    $smtp->datasend("\n");
+    $smtp->datasend($msg);
+    $smtp->dataend();
+
+    $smtp->quit();
+
+    say "Successfully send mail notification";
+    
+    return;
 
 }
 
@@ -71,24 +136,28 @@ sub random_pw {
 
 sub generate_email_text {
 
-    my ($name, $mail, $pw) = @_;
+    my ($template, $name, $user, $email, $org, $pw) = @_;
 
-    return <<"MAIL";
-$name,
+    my $msg;
 
-An account has been created for you on the $org Galaxy server. Your username
-is your email address (all lowercase) and your password is:
+    open my $tpl, '<', $template
+        or die "Error opening template file: $!\n";
 
-$pw
+    warn "$name, $org, $pw\n";
 
-Please change your password after logging in for the first time. If you have
-any questions or have problems logging in, please let me know.
+    while (my $line = <$tpl>) {
+        for my $token (
+            [ 'NAME'  => $name  ],
+            [ 'USER'  => $user  ],
+            [ 'EMAIL' => $email ],
+            [ 'ORG'   => $org   ],
+            [ 'PW'    => $pw    ],
+        ) {
+            $line =~ s/<<<<$token->[0]>>>>/$token->[1]/;
+        }
+        $msg .= $line;
+    }
 
-Regards,
-Jeremy
-MAIL
+    return $msg;
 
 }
-
-
-
